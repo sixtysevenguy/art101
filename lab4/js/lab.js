0ef4587ac12f27bc41326bf2d807c14c26f018ce
sixtysevenguy <130010817+sixtysevenguy@users.noreply.github.com>
2023-04-24 20:26:00 -0700

    Lab 4
    Working on Lab 4.

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 4 Task 3
+// Author: Blake Dringenberg
+// Date: April 24, 2023
 
 // Constants
 
@@ -19,3 +19,15 @@ function main() {
 
 // let's get this party started
 main();
+
+//Create a list of options: rock, paper, and scissors
+    //Ask the player to choose an option
+    //Generate a random option for the computer
+//Compare the player's choice to the computer's choice
+    //If the player's choice beats the computer's choice, display "You win!"
+    //If the computer's choice beats the player's choice, display "Computer wins!"
+    //If it's a tie, display "It's a tie!"
+//Ask the player if they want to play again
+    //If the player chooses to play again, go back to step 2
+    //If the player chooses to quit, end the game.
+
