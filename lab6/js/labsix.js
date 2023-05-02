c04fd4bd52aa7b3df815210d4e4664f7636942d3
sixtysevenguy <130010817+sixtysevenguy@users.noreply.github.com>
2023-05-01 19:44:29 -0700

    Lab 6 - Working on assignment

---
+++ lab6/js/labsix.js
@@ -0,0 +1,40 @@
+// index.js - Lab 6 
+// Author: Blake Dringenberg  
+// Date: April 27th, 2023
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
+
+//define variables
+var mytransport = ["skateboard", "car", "bus", "bike"]
+
+var mymainride = {
+  make: "Honda",
+    model: "Civic",
+    color: "Black",
+    year: "2011",
+}
+
+//output
+document.writeln("Kinds of Transportation I use: ", mytransport, "</br>");
+
+document.writeln("My Main Ride: <pre>",
+      JSON.stringify(mymainride, null, '\t'), "<pre>");
+
+      
+
