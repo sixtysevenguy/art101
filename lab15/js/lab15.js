4a0041f068927d1d1fded5e5dbf39592ddbf1c88
sixtysevenguy <130010817+sixtysevenguy@users.noreply.github.com>
2023-05-31 20:04:04 -0700

    Doing and completeing lab 15

--- lab15/js/lab15.js
+++ lab15/js/lab15.js
@@ -1,4 +1,27 @@
-// index.js - purpose and description here
+// index.js - Lab 15
 // Author: Blake Dringenberg
-// Date:
+// Date:5/31/23
 
+
+
+$(document).ready(function() {
+    // Add click event to the button
+    $('#button').click(function() {
+      // Make the AJAX call
+      $.ajax({
+        url: "https://api.chucknorris.io/jokes/random",
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+          // Convert data to a string
+          var jsonString = JSON.stringify(data.value);
+
+          // Put the output in the output div
+          $('#output').text(jsonString);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.log("Error:", textStatus, errorThrown);
+        }
+      });
+    });
+  }); 
